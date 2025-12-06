eeb7cda87a42be26eb4c0c4663efa1724057dc68
Elman218 <2110310@iub.edu.bd>
2025-12-07 00:22:09 +0600

    deleted other teams project from my branch

--- src/main/java/com/group15/oop_project2/NOOR/User7controller.java
+++
@@ -1,7 +0,0 @@
-package com.group15.oop_project2.NOOR;
-
-public class User7controller
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.group15.oop_project2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.group15.oop_project2 to javafx.fxml;
-    exports com.group15.oop_project2;
-}
