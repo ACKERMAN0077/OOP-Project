dc7429a467b88476c8d43f55298b777c481dbb59
Arbazhossain911 <arbazhossain0@gmail.com>
2025-12-24 07:59:17 +0600

    AdminFinance login working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,6 @@ module mainpkg.ecoresort {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens mainpkg.ecoresort to javafx.fxml;
-    opens mainpkg.ecoresort.Housekeeping to javafx.fxml;
-    opens mainpkg.ecoresort.Restaurant to javafx.fxml;
-    exports mainpkg.ecoresort;
-    exports mainpkg.ecoresort.Housekeeping;
-    exports mainpkg.ecoresort.Restaurant;
+    exports mainpkg.ecoresort.AdminFinance;
+    opens mainpkg.ecoresort.AdminFinance to javafx.fxml;
 }
